de388dd8e2f94a425f67cb1c6e80b170e85778dd
sgonzalezb <sergiogonz48@gmail.com>
2024-06-05 20:15:16 +0200

    Initial commit

---
+++ poc-neo4j-service/src/main/java/edu/pocs/sgonzalezb/poc_neo4j_service/PocNeo4jServiceApplication.java
@@ -0,0 +1,13 @@
+package edu.pocs.sgonzalezb.poc_neo4j_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocNeo4jServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocNeo4jServiceApplication.class, args);
+	}
+
+}
---
+++ poc-neo4j-service/src/test/java/edu/pocs/sgonzalezb/poc_neo4j_service/PocNeo4jServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.pocs.sgonzalezb.poc_neo4j_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocNeo4jServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
