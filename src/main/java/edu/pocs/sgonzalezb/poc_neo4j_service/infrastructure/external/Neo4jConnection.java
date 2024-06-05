a39cf2992b40ead26ce7fdd7d98be7cb785fdbea
sgonzalezb <sergiogonz48@gmail.com>
2024-06-05 21:15:12 +0200

    Feature: implements Neo4j Database connection

---
+++ src/main/java/edu/pocs/sgonzalezb/poc_neo4j_service/infrastructure/external/Neo4jConnection.java
@@ -0,0 +1,28 @@
+package edu.pocs.sgonzalezb.poc_neo4j_service.infrastructure.external;
+
+import org.neo4j.driver.AuthTokens;
+import org.neo4j.driver.Driver;
+import org.neo4j.driver.GraphDatabase;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Neo4jConnection {
+
+    @Value("${neo4j.datasources.uri}")
+    private String uri;
+    @Value("${neo4j.datasources.username}")
+    private String user;
+    @Value("${neo4j.datasources.password}")
+    private String password;
+
+    @Bean
+    public Driver driver() {
+        try (final var driver = GraphDatabase.driver(this.uri, AuthTokens.basic(this.user, this.password))) {
+            driver.verifyConnectivity();
+
+            return driver;
+        }
+    }
+}
