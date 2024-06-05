9ba3307121783d333cee317686f3c2e7ef75b04c
sgonzalezb <sergiogonz48@gmail.com>
2024-06-05 21:36:23 +0200

    Fix: define repository tests in repository test class and include test container dependency

--- src/test/java/edu/pocs/sgonzalezb/poc_neo4j_service/infrastructure/external/Neo4jRepositoryTest.java
+++ src/test/java/edu/pocs/sgonzalezb/poc_neo4j_service/infrastructure/external/Neo4jRepositoryTest.java
@@ -1,5 +1,27 @@
 package edu.pocs.sgonzalezb.poc_neo4j_service.infrastructure.external;
 
+import org.junit.jupiter.api.Test;
+
 class Neo4jRepositoryTest {
+
+    @Test
+    void when_username_is_passed_and_exists_should_return_all_the_directly_users_relationships() {
+
+    }
+
+    @Test
+    void when_username_is_passed_and_not_exists_should_return_empty() {
+
+    }
+
+    @Test
+    void when_username_is_passed_and_have_only_a_directly_boss_should_return_directly_boss_name() {
+
+    }
+
+    @Test
+    void when_username_is_passed_and_have_a_boss_which_also_have_another_boss_should_return_superior_boss_name() {
+
+    }
 
 }
