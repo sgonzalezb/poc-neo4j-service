d5b68fc5186fafb6366b7e6348a3975c6629e653
sgonzalezb <sergiogonz48@gmail.com>
2024-06-12 19:54:11 +0200

    feature: create the UserServiceImpl and define first wrong test (need to be implemented and refactor)

--- src/main/java/edu/pocs/sgonzalezb/poc_neo4j_service/application/UserServiceImpl.java
+++ src/main/java/edu/pocs/sgonzalezb/poc_neo4j_service/application/UserServiceImpl.java
@@ -1,4 +1,37 @@
 package edu.pocs.sgonzalezb.poc_neo4j_service.application;
 
-public class UserServiceImpl {
+import edu.pocs.sgonzalezb.poc_neo4j_service.domain.user.model.User;
+import edu.pocs.sgonzalezb.poc_neo4j_service.domain.user.repository.UserRepository;
+import edu.pocs.sgonzalezb.poc_neo4j_service.domain.user.service.UserService;
+
+import java.util.List;
+
+public class UserServiceImpl implements UserService {
+
+    private UserRepository userRepository;
+
+    @Override
+    public User findUserByName(final String name) {
+        return null;
+    }
+
+    @Override
+    public List<String> findDependentsNamesByUserName(final String name) {
+        return List.of();
+    }
+
+    @Override
+    public String findCompanyNameByUserName(final String name) {
+        return "";
+    }
+
+    @Override
+    public String findDepartmentNameByUserName(final String name) {
+        return "";
+    }
+
+    @Override
+    public String findProjectNameByUserName(final String name) {
+        return "";
+    }
 }
--- src/test/java/edu/pocs/sgonzalezb/poc_neo4j_service/application/UserServiceImplTest.java
+++ src/test/java/edu/pocs/sgonzalezb/poc_neo4j_service/application/UserServiceImplTest.java
@@ -1,12 +1,38 @@
 package edu.pocs.sgonzalezb.poc_neo4j_service.application;
 
+import edu.pocs.sgonzalezb.poc_neo4j_service.domain.user.model.User;
+import edu.pocs.sgonzalezb.poc_neo4j_service.domain.user.repository.UserRepository;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.LocalDate;
+
+import static org.assertj.core.api.BDDAssertions.then;
+
+@ExtendWith(MockitoExtension.class)
 class UserServiceImplTest {
+
+    @Mock
+    private UserRepository userRepository;
+    @InjectMocks
+    private UserServiceImpl userService;
 
     @Test
     void given_valid_username_then_should_return_the_name_of_the_user() {
 
+        //FIXME: apply ObjectMother pattern for test
+        final User expected = new User("Alice", "alice@techcorp.com", LocalDate.parse("2022-01-15"), "Engineer");
+
+        BDDMockito.given(this.userRepository.findUserByName("Alice"))
+                .willReturn(expected);
+
+        final User alice = this.userService.findUserByName("Alice");
+
+        then(alice).isEqualTo(expected);
     }
 
 
